8ab5b2799315c2e6c92cad44985aad1543876f4d
Hatake <ratishatirishvili55@gmail.com>
2023-06-06 18:55:09 +0400

    Task4

---
+++ Task3Homework/Script/apiTesting.js
@@ -0,0 +1,43 @@
+﻿function httpGetRequest()
+{
+  var address = "https://www.boredapi.com/api/activity?/";
+  var aqHttpRequest = aqHttp.CreateGetRequest(address);
+  var aqHttpResponse = aqHttpRequest.Send();
+  
+  var jsonResponse = JSON.parse(aqHttpResponse);
+  var returnedKey = jsonResponse.key;
+  
+  Log.Message(returnedKey);
+  return returnedKey;
+
+  
+}
+
+function musicActivity(){
+  var musicAddress = "http://www.boredapi.com/api/activity?type=music";
+  var request = aqHttp.CreateGetRequest(musicAddress);
+  var response = request.Send();
+  
+  var jsonResponse = JSON.parse(response);
+  var returnedKey = jsonResponse.activity;
+
+  Log.Message(returnedKey);
+  return returnedKey;
+
+}
+function checkignRadioBox()
+{
+  let groupBox = Aliases.Orders.OrderForm.Group;
+  aqObject.CheckProperty(groupBox.Visa, "Enabled", cmpEqual, true);
+  aqObject.CheckProperty(groupBox, "Enabled", cmpEqual, true);
+}
+
+function running(){
+  KeywordTests.OpeningOrder.Run();
+  KeywordTests.NewOrder.Run();
+  Aliases.Orders.OrderForm.Group.Customer.Keys(musicActivity());
+  Aliases.Orders.OrderForm.Group.Zip.Keys(httpGetRequest());
+  checkignRadioBox();
+  KeywordTests.ClosingOrder.Run();
+}
+
---
+++ Task3Homework/Script/testingOrder.js
@@ -0,0 +1,36 @@
+﻿var min = 0;
+var max = 1000000;
+var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+let orders = Aliases.Orders;
+let mainForm = orders.MainForm;
+let orderForm = orders.OrderForm;
+let groupBox = orderForm.Group;
+let okButton = orderForm.ButtonOK;
+let cardText = groupBox.CardNo;
+let listView = mainForm.OrdersView;
+
+function FillRandom(){
+  KeywordTests.OpeningOrder.Run();
+  KeywordTests.NewOrder.Run();
+  cardText.Click();
+  cardText.Keys(randomNumber+"^A^C");
+  var val = Sys.Clipboard;
+  Log.Message(val);
+  okButton.Click();
+  listView.DblClickItem(0, "MyMoney");
+  cardText.Click();
+  var new_val = "924923"
+  cardText.Keys("^A"+ new_val);
+  okButton.Click();
+  listView.DblClickItem(0, "MyMoney");
+  aqObject.CheckProperty(cardText, "wText", cmpEqual, new_val);
+  okButton.Click();
+  mainForm.OrdersView.ClickItem(0, "MyMoney");
+  let toolBar = mainForm.ToolBar;
+  aqObject.CheckProperty(toolBar, "Enabled", cmpEqual, true);
+  toolBar.ClickItem(5, false);
+  aqObject.CheckProperty(orders.OrderForm.Group, "Enabled", cmpEqual, true);
+  
+}
+
